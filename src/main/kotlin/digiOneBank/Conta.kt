03df38532d887d3162bb93bf4ab7cd4a34a3110a
Marcelo-San <marcelosantos.empreendedor@gmail.com>
2021-04-07 21:03:57 -0300

    Classe Conta criada como construtor primário.

--- src/main/kotlin/digiOneBank/Conta.kt
+++ src/main/kotlin/digiOneBank/Conta.kt
@@ -1,4 +1,18 @@
 package digiOneBank
 
-class Conta {
+import java.math.BigDecimal
+
+// Classe conta já criada como construtor primário.
+class Conta(
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
 }
