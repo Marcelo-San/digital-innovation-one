d06bbbb6a8e580ccc74e99bc8e3ea2399352c261
Marcelo-San <marcelosantos.empreendedor@gmail.com>
2021-04-12 20:23:45 -0300

    Criado enum 'ClienteTipo' e o seu teste com 'It'.

--- src/main/kotlin/digiOneBank/testes/ClienteTipoEnumTeste.kt
+++ src/main/kotlin/digiOneBank/testes/ClienteTipoEnumTeste.kt
@@ -4,12 +4,18 @@ import digiOneBank.ClienteTipo
 
 fun main() {
     ClienteTipo.values().forEach {
-        println("${it.name} - $it")
+        println("${it.name} - ${it.descricao}") // A própria IDE coloca as chaves para nós.
     }
 
-    // Ou poderia ser assim, como abaixo:
+    // Ou poderia usar um nome de nossa preferência, por exemplo, como abaixo:
 
     ClienteTipo.values().forEach { elemento ->
-        println("${elemento.name} - $elemento")
+        println("${elemento.name} - ${elemento.descricao}") // A própria IDE coloca as chaves para nós.
     }
+
+    val pf = ClienteTipo.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">> ${pj.name} - ${pj.descricao}")
 }
